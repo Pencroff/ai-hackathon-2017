cbd8ec348521b2951fa450d00ef6cab476d7b2e8
PabloPalomar <palomar.pastor@gmail.com>
2017-02-18 14:26:09 +0000

    Changed main method to return the data returned by get_product_details

--- flask_main.py
+++ flask_main.py
@@ -8,9 +8,9 @@ api_key = 'DEMO_KEY'
 
 
 @app.route("/compare/<param_food_name>", methods=['GET'])
-def compare(param_food_name):
-    # food_collection = get_product_list(param_food_name)
-    food_collection = [(0, 'whatever'), (1, 'other thing')]
+def get_food_details(param_food_name):
+    food_collection = get_product_list(param_food_name)
+    import pdb; pdb.set_trace()
     optimum_result = 0
     best_ratio = 0
     for t in food_collection:
@@ -20,19 +20,18 @@ def compare(param_food_name):
             best_ratio = ratio
             optimum_result = food_id
 
-    # food_elements = r.get('http://example.com').content
-    # return food_elements
-    return str(optimum_result)
+    return get_product_detail(optimum_result)
 
 
 def get_product_list(q=''):
 #   https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=DEMO_KEY
     url_root = 'https://api.nal.usda.gov/ndb/search/?q={query}&format={format}&sort={sort}&max={records_limit}&&offset={offset}&api_key={api_key}'
-    params = { 'format': 'json', 'offset': 0, 'records_limit': 10, 'sort': 'n', 'api_key': api_key, 'query': q }
+    params = {'format': 'json', 'offset': 0, 'records_limit': 10, 'sort': 'n', 'api_key': api_key, 'query': q}
     resp = r.get(url_root.format_map(params))
     data = json.loads(resp._content)
     result = list([(x['ndbno'], x['name']) for x in data['list']['item']])
     return result
+
 
 def get_product_detail(product_id):
 #   https://api.nal.usda.gov/ndb/reports/?ndbno=01009&type=f&format=json&api_key=DEMO_KEY
